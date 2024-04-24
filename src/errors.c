7ce97519e4025f178c421c6a8783359482030dbb
Timothée Lafay <87896378+Tlafay1@users.noreply.github.com>
2024-04-24 07:48:28 +0000

    Starting to test but bad behavior in libargparse

--- src/errors.c
+++ src/errors.c
@@ -12,44 +12,51 @@
 
 #include "ft_nm.h"
 
-
-/*
-	Checks if the file can be opened sucessfully
-*/
-
-int	check_filetype(mode_t m, const char *prog_name, const char *path)
+/**
+ * Checks if the file can be opened successfully.
+ *
+ * @param m         The file mode.
+ * @param prog_name The name of the program.
+ * @param path      The path of the file.
+ * @return          1 if the file is not valid, 0 otherwise.
+ */
+int check_filetype(mode_t m, const char *prog_name, const char *path)
 {
-	char	*message = NULL;
+	char *message = NULL;
 
 	if (S_ISDIR(m))
 		message = "is a directory";
-	
 	else if (!S_ISREG(m))
 		message = "is not an ordinary file";
-	
+
 	if (message)
 		printf("%s: Warning: '%s' %s\n", prog_name, path, message);
-	
+
 	return !(message == NULL);
 }
 
-
-/*
-	Error message when opening the file
-*/
-
-int	nm_error(const char *prog_name, const char *path, char *msg)
+/**
+ * Prints an error message when opening the file.
+ *
+ * @param prog_name The name of the program.
+ * @param path      The path of the file.
+ * @param msg       The error message.
+ * @return          1.
+ */
+int nm_error(const char *prog_name, const char *path, char *msg)
 {
 	printf("%s: %s: %s\n", prog_name, path, msg);
 	return (1);
 }
 
-
-/*
-	Error returned when a syscall failed
-*/
-
-int	syscall_error(const char *prog_name, const char *path)
+/**
+ * Prints an error message when a syscall fails.
+ *
+ * @param prog_name The name of the program.
+ * @param path      The path of the file.
+ * @return          1.
+ */
+int syscall_error(const char *prog_name, const char *path)
 {
 	if (errno == ENOENT)
 		printf("%s: '%s': No such file\n", prog_name, path);
@@ -57,4 +64,3 @@ int	syscall_error(const char *prog_name, const char *path)
 		printf("%s: Warning: '%s' is not an ordinary file\n", prog_name, path);
 	return (1);
 }
-
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,28 +1,13 @@
 #include "tests.hpp"
 
-class BinariesTests : public ::testing::Test
+TEST(ReadFile, FileDoesntExist)
 {
+    const char *argv[] = {"./ft_nm", "binaries/dontexist", NULL};
+    ASSERT_EQ(ft_nm_main(argv), 1);
+}
 
-protected:
-    void SetUp() override
-    {
-        std::cout << "SetUp" << std::endl;
-    }
-
-    void TearDown() override
-    {
-        std::cout << "TearDown" << std::endl;
-    }
-};
-
-TEST(BinariesTests, ParseArguments)
+TEST(ReadFile, FileIsDirectory)
 {
-    const char *args[3] = {"./ft_nm", "tests/binaries/valid", nullptr};
-    testing::internal::CaptureStdout();
-    ft_nm_main(args);
-    std::string output = testing::internal::GetCapturedStdout();
-    testing::internal::CaptureStdout();
-    system("nm tests/binaries/valid");
-    std::string expected = testing::internal::GetCapturedStdout();
-    ASSERT_EQ(output, expected);
+    const char *argv[] = {"./ft_nm", "binaries", NULL};
+    ASSERT_EQ(ft_nm_main(argv), 1);
 }
